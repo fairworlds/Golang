a0358db56fb65c1ae50e25ea9608f202e1049ba2
Tikhon Kharnavtcov <t.kharnavtsov@energo.app>
2023-12-19 22:03:42 +0300

    dz is completed

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,29 +1,30 @@
-package init
+package main
 
 import (
+	"fmt"
+
 	"github.com/fixme_my_friend/hw02_fix_app/printer"
 	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
-	"fmt"
 )
 
-func init() {
-	var path string = "data.json"
+func main() {
+	var path = "/home/fair/github/data.json"
 
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
 
-	var err error
-	var staff []types.Employee
+	///var err error
+	///var staff []types.Employee
 
 	if len(path) == 0 {
-		path = "data.json"
-	} else {
+		path = "/home/fair/github/data.json"
 	}
 
-	staff, err = reader.ReadJSON(path, -1)
-
-	fmt.Print(err)
+	var staff, err = reader.ReadJSON(path)
+	if err != nil {
+		fmt.Print(err)
+	}
+	///fmt.Print(err)
 
 	printer.PrintStaff(staff)
 }
