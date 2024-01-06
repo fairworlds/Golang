1aefefc9f4efa87edde2c71b7eff00a7d1db40a9
Kharnavcov Tikhon <xarnavcov@gmail.com>
2024-01-06 09:45:06 +0200

    new branch

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"bufio" //пакет для чтения ввода с клавиатуры
+	"bufio" // пакет для чтения ввода с клавиатуры
 	"fmt"
-	"os"      //для работы операционной системой
+	"os"      // для работы операционной системой
 	"strconv" // преобразрование из строки в число
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin) //считываем с клавиатуры и сохраняем в sizereader
+	reader := bufio.NewReader(os.Stdin) // считываем с клавиатуры и сохраняем в sizereader
 
 	fmt.Print("Введите размер доски: ")
 	sizereader, _ := reader.ReadString('\n')
 	sizereader = sizereader[:len(sizereader)-1] // удаляем символ новой строки Len возвращает длину
-	//строки или кода len(sizereader) возвращает длину переменной sizereader
+	// строки или кода len(sizereader) возвращает длину переменной sizereader
 
 	sizechess, err := strconv.Atoi(sizereader) // преобразуем данные в целое число при этом
 	// atoi - это функция в пакете strconv, которая преобразует строку в целое число (integer). В данном случае strconv.Atoi(sizereader) преобразует значение переменной sizereader из строки в целое число (integer).
-	//Если преобразование не удалось, функция возвращает ошибку
+	// Если преобразование не удалось, функция возвращает ошибку
 	if err != nil {
 		fmt.Println("Ошибка при чтении размера доски:", err)
 		return
