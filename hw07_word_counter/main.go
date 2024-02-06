9e80ebc2a703924576aa52f6fe337cbb51e17e16
Tikhon Kharnavtcov <t.kharnavtsov@energo.app>
2024-02-07 00:23:12 +0300

    mb fix

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -8,10 +8,17 @@ import (
 	"strings"
 )
 
-func countWorld(row []string) map[string]int {
+func countWorld(row string) map[string]int { // прием строки в качестве аргумента
 	wordDict := make(map[string]int)
-	for _, text := range row {
-		wordDict[strings.ToLower(text)]++
+	words := strings.Fields(row)
+
+	for _, text := range words {
+		_, ok := wordDict[strings.ToLower(text)]
+		if !ok {
+			wordDict[strings.ToLower(text)] = 1
+		} else {
+			wordDict[strings.ToLower(text)]++
+		}
 	}
 	return wordDict
 }
@@ -20,24 +27,24 @@ func WordSplit(row string) ([]string, error) {
 	text := []string{}
 
 	if len(strings.TrimSpace(row)) == 0 {
-		return nil, errors.New("empty string")
+		return []string{""}, errors.New("передали пустую строку")
 	}
-
 	splitFunc := func(r rune) bool {
 		return strings.ContainsRune("*%,_!.' ''\n'", r)
 	}
-
 	words := strings.FieldsFunc(row, splitFunc)
 	text = append(text, words...)
 	return text, nil
 }
 
 func main() {
-	fmt.Println("Введите текст")
+	fmt.Println("Введите строку:")
 	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	splitworld, err := WordSplit(text)
+	splitWords, err := WordSplit(text)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(countWorld(splitworld))
+	fmt.Println("Разделенные слова:", splitWords)
+	wordCount := countWorld(text)
+	fmt.Println("Количество повторений слов:", wordCount)
 }
